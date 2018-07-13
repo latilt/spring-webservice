de29e9846570678cc3f90e08b2d3dcfcedca5517
latilt <latilt2@gmail.com>
2018-07-14 01:29:55 +0900

    SpringBoot & JPA 활용 간단한 API 생성

--- src/main/java/com/latilt/webservice/web/WebRestController.java
+++ src/main/java/com/latilt/webservice/web/WebRestController.java
@@ -1,13 +1,28 @@
 package com.latilt.webservice.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.latilt.webservice.domain.posts.PostsRepository;
+import com.latilt.webservice.dto.posts.PostsSaveRequestDto;
+
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+	
+	private PostsRepository postsRepository;
 	
 	@GetMapping("/hello")
 	public String hello() {
-		return "HelloWorld";
+		return "HelloWorld2";
+	}
+	
+	@PostMapping("/posts")
+	public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+		postsRepository.save(dto.toEntity());
 	}
 }
