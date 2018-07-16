9db166187496dbd4c68fe72c0ca66c3ec1ff204e
latilt <latilt2@gmail.com>
2018-07-17 00:13:27 +0900

    Handlebars 연동

---
+++ src/main/java/com/latilt/webservice/web/WebController.java
@@ -0,0 +1,17 @@
+package com.latilt.webservice.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+public class WebController {
+
+	@GetMapping("/")
+	public String main() {
+		return "main";
+	}
+}
