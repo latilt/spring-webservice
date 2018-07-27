6f9a830bd3f77cb26e63a7c2d6516217a0c331c6
latilt <latilt2@gmail.com>
2018-07-28 01:17:52 +0900

    [edit] application.java

--- src/main/java/com/latilt/webservice/Application.java
+++ src/main/java/com/latilt/webservice/Application.java
@@ -2,6 +2,7 @@ package com.latilt.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -9,10 +10,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
 	
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml,"
-			+ "/app/config/spring-webservier/real-applocation.yml";
+            + "classpath:application.yml,"
+            + "/app/config/spring-webservice/real-application.yml";
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+        new SpringApplicationBuilder(Application.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
+    }
 }
