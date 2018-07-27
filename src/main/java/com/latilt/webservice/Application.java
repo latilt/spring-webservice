749e4a004a214db661b9600e7e6cdf8d809b7148
latilt <latilt2@gmail.com>
2018-07-28 00:03:47 +0900

    [add] nginx

--- src/main/java/com/latilt/webservice/Application.java
+++ src/main/java/com/latilt/webservice/Application.java
@@ -7,6 +7,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
+	
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/app/config/spring-webservier/real-applocation.yml";
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/main/java/com/latilt/webservice/web/WebRestController.java
+++ src/main/java/com/latilt/webservice/web/WebRestController.java
@@ -1,5 +1,8 @@
 package com.latilt.webservice.web;
 
+import java.util.Arrays;
+
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +20,7 @@ public class WebRestController {
 	
 	private PostsRepository postsRepository;
 	private PostsService postsService;
+	private Environment env;
 	
 	@GetMapping("/hello")
 	public String hello() {
@@ -26,5 +30,12 @@ public class WebRestController {
 	@PostMapping("/posts")
 	public Long savePosts(@RequestBody PostsSaveRequestDto dto) {
 		return postsService.save(dto);
+	}
+	
+	@GetMapping("/profile")
+	public String getProfile() {
+		return Arrays.stream(env.getActiveProfiles())
+				.findFirst()
+				.orElse("");
 	}
 }
